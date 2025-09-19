f29a6593e5f048ddc6883b43df0afb047a947584
Soseki <florentins1919@gmail.com>
2025-09-19 16:45:34 +0900

    Update comments in EnvTest

--- src/test/java/test_fword_reporter/EnvTest.java
+++ src/test/java/test_fword_reporter/EnvTest.java
@@ -2,9 +2,12 @@ package test_fword_reporter;
 
 import io.github.cdimascio.dotenv.Dotenv;
 
+/**
+ * トークン設定テスト
+ */
 public class EnvTest {
     public static void main(String[] args) {
         Dotenv dotenv = Dotenv.load();
-        System.out.println(dotenv.get("TOKEN")); // .env に TOKEN=xxx があれば表示される
+        System.out.println(dotenv.get("TOKEN"));
     }
 }
